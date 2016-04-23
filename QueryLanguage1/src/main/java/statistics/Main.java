4bb30772506a3d91952678f154e10d920ffa8276
Arto Rantanen <arto.rantanen@helsinki.fi>
2016-04-23 20:51:37 +0300

    4

--- QueryLanguage1/src/main/java/statistics/Main.java
+++ QueryLanguage1/src/main/java/statistics/Main.java
@@ -5,16 +5,22 @@ import statistics.matcher.*;
 public class Main {
     public static void main(String[] args) {
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
-          
-        Matcher m = new And( new HasFewerThan(10, "goals"),
+        QueryBuilder qb = new QueryBuilder();
+        Matcher m = new And( new HasAtLeast(10, "goals"),
                              new HasAtLeast(10, "assists"),
                              new PlaysIn("PHI")
         );
         printPlayers(stats,m);
-        m = new Or( new HasFewerThan(10, "goals"),
-                             new HasAtLeast(10, "assists"),
-                             new PlaysIn("PHI")
-        );
+        m = qb.hasAtLeast(10,"goals").hasAtLeast(10, "assists").playsIn("PHI").build();
+        printPlayers(stats,m);
+        m = qb.oneOf(
+                        qb.playsIn("PHI")
+                             .hasAtLeast(10, "goals")
+                             .hasFewerThan(15, "assists").build(),
+
+                        qb.playsIn("EDM")
+                             .hasAtLeast(50, "points").build()
+                       ).build();
         printPlayers(stats,m);
     }
     private static void printPlayers(Statistics stats, Matcher m ){
---
+++ QueryLanguage1/src/main/java/statistics/matcher/QueryBuilder.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistics.matcher;
+
+
+/**
+ *
+ * @author arto
+ */
+public class QueryBuilder {
+    Matcher matcher;
+    
+    public QueryBuilder(){
+        
+    }
+    public QueryBuilder playsIn(String team){
+        
+        changeMatcher(new PlaysIn(team));
+        return this;
+    }
+    public QueryBuilder hasAtLeast(int value, String category){
+        changeMatcher(new HasAtLeast(value,category));
+        return this;
+    }
+    public QueryBuilder oneOf(Matcher...m){
+        matcher = new Or(m);
+        return this;
+    }
+    public QueryBuilder hasFewerThan(int value, String category){
+        changeMatcher(new HasFewerThan(value,category));
+        return this;
+    }
+    public Matcher build(){
+        Matcher output = this.matcher;
+        this.matcher = null;
+        return output;
+    }
+    private void changeMatcher(Matcher m){
+        if(this.matcher == null){
+            this.matcher = m;
+        }else {
+            this.matcher = new And(matcher,m);
+        }
+        
+    }
+}
